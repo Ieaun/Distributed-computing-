2ee3ade0d022ca468ac16c30a5803c01991c9894
Ieaun <52431013+Ieaun@users.noreply.github.com>
2020-03-06 18:05:35 +0000

    Added basic UI elements to view transcripts and additional information on each

---
+++ Project/Transcript_Repository/Transcript_Repository/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Transcript_Repository.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Transcripts()
+        {
+            ViewBag.Message = "Your transcripts page.";
+
+            return View();
+        }
+
+        public ActionResult Actions()
+        {
+            ViewBag.Message = "Your actions page.";
+
+            return View();
+        }
+
+        public ActionResult Detailed_Transcript()
+        {
+            ViewBag.Message = "Your detailed transcript page.";
+
+            return View();
+        }
+
+    }
+}
